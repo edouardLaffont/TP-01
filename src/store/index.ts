630a15ceb1d33109a9dc29014920484abc0bc9bf
edouardLaffont <edouard.laffont@live.com>
2023-04-19 17:15:16 +0200

    feat(src/front): create todo ui and add vuex store data

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,41 @@
 import { createStore } from "vuex";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+export interface Task {
+  name: string;
+  done: boolean;
+}
+
+export interface State {
+  tasks: Task[];
+}
+
+export default createStore<State>({
+  state: {
+    tasks: [],
+  },
+  mutations: {
+    addTask(state, task: Task) {
+      state.tasks.push(task);
+    },
+    deleteTask(state, index: number) {
+      state.tasks.splice(index, 1);
+    },
+    updateTask(state, payload: { index: number; task: Task }) {
+      state.tasks[payload.index] = payload.task;
+    },
+  },
+  actions: {
+    addTask({ commit }, task: Task) {
+      commit("addTask", task);
+    },
+    deleteTask({ commit }, index: number) {
+      commit("deleteTask", index);
+    },
+    updateTask({ commit }, payload: { index: number; task: Task }) {
+      commit("updateTask", payload);
+    },
+  },
+  getters: {
+    getTasks: (state) => state.tasks,
+  },
 });

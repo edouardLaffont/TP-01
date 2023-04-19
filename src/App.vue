f354be2e424ac615f4a400b5f8d12219ea377ab8
edouardLaffont <edouard.laffont@live.com>
2023-04-19 17:15:16 +0200

    feat(src/front): create todo ui and add vuex store data

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <h1>Todolist</h1>
+    <form @submit.prevent="addTask">
+      <input type="text" v-model="task" placeholder="Add task" />
+      <button type="submit">Add</button>
+    </form>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <input
+          type="checkbox"
+          :id="index.toString()"
+          :checked="task.done"
+          @change="updateTask({ name: task.name, done: !task.done }, index)"
+        />
+        <label :for="index.toString()" :class="{ done: task.done }">{{
+          task.name
+        }}</label>
+        <button @click="deleteTask(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, InjectionKey } from "vue";
+import { Task, State } from "@/store";
+import { useStore, Store } from "vuex";
 
-@Options({
-  components: {
-    HelloWorld,
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      task: "",
+    };
   },
-})
-export default class App extends Vue {}
+  computed: {
+    tasks(): Task[] {
+      return this.$store.getters.getTasks;
+    },
+  },
+  methods: {
+    addTask() {
+      if (this.task) {
+        const task: Task = { name: this.task, done: false };
+        this.$store.dispatch("addTask", task);
+        this.task = "";
+      }
+    },
+    deleteTask(index: number) {
+      this.$store.dispatch("deleteTask", index);
+    },
+    updateTask(task: Task, index: number) {
+      this.$store.dispatch("updateTask", { task, index });
+    },
+  },
+});
+
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $store: Store<State>;
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.done {
+  text-decoration: line-through;
 }
 </style>
